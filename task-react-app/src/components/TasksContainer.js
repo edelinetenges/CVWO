d77117693d95fc06520993470223cab63d4f9562
edelinetenges <edelinetenges@gmail.com>
2020-01-21 15:47:04 +0800

    loaded data from backend

--- task-react-app/src/components/TasksContainer.js
+++ task-react-app/src/components/TasksContainer.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class TasksContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: []
+        }
+    }
+
+    getTasks() {
+        axios.get('/api/v1/tasks')
+        .then(response => {
+            this.setState({tasks: response.data})
+        })
+        .catch(error => console.log(error))
+    }
+
+    componentDidMount() {
+        this.getTasks()
+    }
+
     render() {
         return (
             <div>
                 <div className="inputContainer">
                     <input className="taskInput" type="text"
-                    placeholder="Add task" maxLength="50" />
+                    placeholder="Add task" maxLength="50" 
+                    onKeyPress={this.createTask} />
                 </div>
                 <div className="listWrapper">
                     <ul className="taskList">
-
+                        {this.state.tasks.map((task) => {
+                            return(
+                                <li className="task" task={task} key={task.id}>
+                                    <input className="taskCheckbox" type="checkbox" />
+                                    <label className="taskLabel">{task.title}</label>
+                                    <span className="deleteTaskBtn">Delete</span>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
